1835399b8cd07e8b79067e8f4e6e0ddea3d8299d
ss-koshala <koshala.manojeewa@silverstripers.com>
2021-08-09 16:48:07 +0530

    fixes

--- code/Forms/GridFieldUserDefinedExportsFieldsDetailForm_ItemRequest.php
+++ code/Forms/GridFieldUserDefinedExportsFieldsDetailForm_ItemRequest.php
@@ -3,7 +3,9 @@ namespace UserDefinedExports\Forms;
 
 use SilverStripe\Forms\GridField\GridFieldDetailForm_ItemRequest;
 use SilverStripe\ORM\ValidationResult;
+use UserDefinedExports\Model\UserDefinedExportsButton;
 use UserDefinedExports\Model\UserDefinedExportsField;
+use UserDefinedExports\Model\UserDefinedExportsItem;
 
 class GridFieldUserDefinedExportsFieldsDetailForm_ItemRequest extends GridFieldDetailForm_ItemRequest {
 
@@ -13,6 +15,7 @@ class GridFieldUserDefinedExportsFieldsDetailForm_ItemRequest extends GridFieldD
 
     public function doSave($data, $form)
     {
+
         $type = isset($data['SelectedType']) ? $data['SelectedType'] : '';
         if($type == 'Pre defined columns') {
 
@@ -26,33 +29,47 @@ class GridFieldUserDefinedExportsFieldsDetailForm_ItemRequest extends GridFieldD
                 return null;
             }
 
-            $class = $this->record->ClassName;
-            $object = new $class();
-            $arr = $object::config()->user_defined_export_column_mapping;
             $button = $this->record->UserDefinedExportsButtonID;
 
-            foreach ($arr as $key => $item) {
-                $exportField = new UserDefinedExportsField();
-                $exportField->SelectedType = $type;
-                $exportField->UserDefinedExportsButton = $button;
-                $exportField->OriginalExportField = $key;
-                $exportField->ExportFieldLabel = $item;
-                $exportField->write();
+            $buttonObject = UserDefinedExportsButton::get()->filter('ID',$button)->first();
+            $exportItem = UserDefinedExportsItem::get()->filter('ID', $buttonObject->UserDefinedExportsItemID)->first();
+
+            $class = $exportItem->ManageModelName;
+            $object = new $class();
+            $arr = $object::config()->user_defined_export_column_mapping;
+            if(!empty($arr)) {
+                foreach ($arr as $key => $item) {
+                    $exportField = new UserDefinedExportsField();
+                    $exportField->SelectedType = $type;
+                    $exportField->UserDefinedExportsButtonID = $button;
+                    $exportField->OriginalExportField = $key;
+                    $exportField->ExportFieldLabel = $item;
+                    $exportField->write();
+                }
+
+                $link = '<a href="' . $this->Link('edit') . '">"'
+                    . htmlspecialchars($this->record->Title, ENT_QUOTES)
+                    . '"</a>';
+                $message = _t(
+                    'SilverStripe\\Forms\\GridField\\GridFieldDetailForm.Saved',
+                    'Saved {name} {link}',
+                    [
+                        'name' => $this->record->i18n_singular_name(),
+                        'link' => $link
+                    ]
+                );
+
+                $messageType = 'good';
+                $result = ValidationResult::CAST_HTML;
+            } else {
+                $message = 'There are no array data';
+                $messageType = 'error';
+                $result = ValidationResult::CAST_TEXT;
             }
 
-            $link = '<a href="' . $this->Link('edit') . '">"'
-                . htmlspecialchars($this->record->Title, ENT_QUOTES)
-                . '"</a>';
-            $message = _t(
-                'SilverStripe\\Forms\\GridField\\GridFieldDetailForm.Saved',
-                'Saved {name} {link}',
-                [
-                    'name' => $this->record->i18n_singular_name(),
-                    'link' => $link
-                ]
-            );
+            $form->sessionMessage($message, $messageType, $result);
 
-            $form->sessionMessage($message, 'good', ValidationResult::CAST_HTML);
+
             // Redirect after save
             $controller = $this->getToplevelController();
             $controller->getRequest()->addHeader('X-Pjax', 'Content');
